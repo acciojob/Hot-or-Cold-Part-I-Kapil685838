8b60fc7f4ecf2f8cb42484f742a37f60364bfbe6
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-03-03 22:22:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-//your code here
-var randomNum;
-var input;
-function randomNumGenerator(){
-	randomNum = Math.floor(Math.random()*41) - 20;
-	document.getElementById("num").innerHTML = randomNum;
-	guessName();
-	print();
-}
-function guessName() {
-	input = parseInt(document.getElementById("guess").value);	
-}
-function print(){
-	if(Math.abs(randomNum - input) <= 5){
-		document.getElementById("respond").innerHTML = "Hot";
-	}
-	else{
-		document.getElementById("respond").innerHTML = "Cold";
-	}
-}
+const userGuess = document.getElementById('guess');
+const btn = document.getElementById('btn');
+const displayNum = document.getElementById('num');
+const hotOrCold = document.getElementById('respond');
+
+btn.addEventListener('click', ()=>{
+    let random = Math.floor((Math.random()*21));
+    let userNum = Number(userGuess.value);
+    displayNum.innerText = random;
+    if(random <= 20 && random >= -20){
+        hotOrCold.innerText = 'Hot';
+    }else{
+        hotOrCold.innerText = 'Cold';
+    }
+})
